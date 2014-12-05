8b83a4cefbb4b503591bc4d3c9b2bb0b5a7aee58
Benjamin Cabé <benjamin@eclipse.org>
2014-12-05 11:48:43 +0100

    fix 365 trends

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var Spreadsheet = require('edit-google-spreadsheet');
 var REGEXP = /iot\.(.+?)-.*evolutionary.json/;
 
 // number of full months elapsed since Jan-1 2001
-var months = moment().startOf('month').subtract(1, 'day').diff(moment([2001, 0, 1]), 'months') + 1;
+var months = moment(). /*startOf('month').subtract(1, 'day').*/ diff(moment([2001, 0, 1]), 'months') + 1;
 
 var SCM_FIELDS = ['authors', 'commits'];
 var ITS_FIELDS = ['opened', 'closed'];
@@ -26,7 +26,7 @@ for (var key in data) {
 		var projectName = key.match(REGEXP)[1];
 		result[projectName] = result[projectName] || {};
 
-		console.log(projectName);
+		console.log(projectName, ' - ', key);
 
 		var fields = []
 
@@ -46,7 +46,7 @@ for (var key in data) {
 		}
 
 		for (month = 0; month < months; month++) {
-			month_str = moment([2001, 0, 1]).add(month, 'month').format('YYYY-MM');
+			month_str = moment([2001, 0, 1]).add(month, 'month').endOf('month').format('YYYY-MM-DD');
 
 			result[projectName][month_str] = result[projectName][month_str] || {}
 
@@ -59,18 +59,34 @@ for (var key in data) {
 }
 
 var spreadsheetRows = [];
-var spreadsheetRows_monthlyVariation = [];
-
 
 var ALL_FIELDS = SCM_FIELDS.concat(ITS_FIELDS).concat(MLS_FIELDS);
 
-var HEADER = ['project', 'date'].concat(ALL_FIELDS);
+var HEADER = ['project', 'date'].concat(ALL_FIELDS).concat(['senders_365', 'percentage_senders_365', 'authors_365', 'percentage_authors_365']);
 
 spreadsheetRows.push(HEADER);
-spreadsheetRows_monthlyVariation.push(HEADER);
 
 for (var p in result) {
 	var project = result[p];
+
+	// try and get the senders_365 & percentage in "iot.{projectName}-mls-prj-static.json"
+	staticMls = data['iot.' + p + '-mls-prj-static.json']
+	var senders_365 = 0,
+		percentage_senders_365 = 0;
+	if (staticMls) {
+		senders_365 = staticMls.senders_365;
+		percentage_senders_365 = staticMls.percentage_senders_365 * (staticMls.diff_netsenders_365 > 0 ? 1 : -1) / 100;
+	}
+
+
+	// try and get the authors_365 & percentage in "iot.{projectName}-scm-prj-static.json"
+	staticScms = data['iot.' + p + '-scm-prj-static.json']
+	var authors_365 = 0,
+		percentage_authors_365 = 0;
+	if (staticScms) {
+		authors_365 = staticScms.authors_365;
+		percentage_authors_365 = staticScms.percentage_authors_365  * (staticScms.diff_netauthors_365 > 0 ? 1 : -1) / 100;
+	}
 
 	for (var d in project) {
 		// start in 2012
@@ -87,6 +103,11 @@ for (var p in result) {
 				row.push(date[f]);
 		})
 
+		row.push(senders_365);
+		row.push(percentage_senders_365);
+		row.push(authors_365);
+		row.push(percentage_authors_365);
+
 		spreadsheetRows.push(row);
 	}
 }
@@ -95,12 +116,12 @@ Spreadsheet.load({
 	debug: false,
 	spreadsheetId: '1MT8vUectDG7qnt83LBts-B7oECMsICXyB4Tn4Kxl64U',
 	worksheetId: 'od6',
-//	username: username,
-//	password: password,	
-	oauth : {
-      email: '994193235180-598b5b2t21ms6nra6c4ej63sv5q2aem9@developer.gserviceaccount.com',
-      keyFile: '/Users/kartben/Downloads/key.pem'
-    },
+	//	username: username,
+	//	password: password,	
+	oauth: {
+		email: '994193235180-598b5b2t21ms6nra6c4ej63sv5q2aem9@developer.gserviceaccount.com',
+		keyFile: '/Users/kartben/Downloads/key.pem'
+	},
 }, function sheetReady(err, spreadsheet) {
 	spreadsheet.add({
 		1: spreadsheetRows
